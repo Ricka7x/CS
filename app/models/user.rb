c3fddc35c68059bc0b518cae913c33d1055fae39
ricka7x <ricky.ramirez89@gmail.com>
2016-04-10 00:02:45 -0500

    added facebook signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,32 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true, length: {maximum: 25}       
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
+
+  validates :name, presence: true, length: {maximum: 25}   
+  validates :email, :uniqueness => true
+
+  def self.find_for_facebook_oauth(access_token, signed_in_resourse=nil)
+    data = access_token.info
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid).first
+
+    if user
+      return user
+    else
+      registered_user = User.where(:email => data.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(
+          name: access_token.extra.raw_info.name,
+          provider: access_token.provider,
+          email: data.email,
+          uid: access_token.uid,
+          image: data.image,
+          password: Devise.friendly_token[0,20],
+        )
+      end
+    end
+  end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
+              :controllers => {:omniauth_callbacks => "omniauth_callbacks"}
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
