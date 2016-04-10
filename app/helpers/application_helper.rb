5080b600db59f6826f1e866ccb319c164b19b926
ricka7x <ricky.ramirez89@gmail.com>
2016-04-09 22:57:13 -0500

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,20 @@
+module ApplicationHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicons&s=35"
+  end 
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
+              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+              concat message 
+            end)
+    end
+    nil
+  end
+end
