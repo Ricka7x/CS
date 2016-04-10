3fb2ba3c38c684bad966e788e5b56ed43c12a471
ricka7x <ricky.ramirez89@gmail.com>
2016-04-10 08:01:57 -0500

    Added active_admin-sortable_tree gem

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -1,19 +1,22 @@
 ActiveAdmin.register Task do
+	permit_params :title, :note, :video, :header, :tag, :project_id
 
-	permit_params :title, :note, :video, :header, :tag, :course_id
+	sortable tree: false,
+						sorting_attribute: :tag
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+	index :as => :sortable do
+		label :title
 
+		actions
+	end
 
+	index do 
+		selectable_column
+		column :header
+		column :title
+		column :tag
+		column :course
+
+		actions
+	end
 end
